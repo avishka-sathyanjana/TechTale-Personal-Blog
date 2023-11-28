a919dccf4ff95ddabcb7a90e3d28485f0a4850c8
Avishka Sathyanjana <baavishka@gmail.com>
2023-11-28 17:02:05 +0530

    feat: add homepage with styles

--- src/App.js
+++ src/App.js
@@ -1,16 +1,93 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <main className="App">
       <header>
-        <a href=' ' className="logo">MyBlog</a>
+        <a href=" " className="logo">
+          MyBlog
+        </a>
         <nav>
           <a href="">Login</a>
           <a href="">Register</a>
         </nav>
       </header>
+
+      <div className="post">
+        <div className="image">
+          <img src="https://serokell.io/files/q5/q5c8rtrc.pasted_image_0_(5).png"></img>
+        </div>
+        <div className="texts">
+          <h2>Introduction to Generative AI</h2>
+          <p className='info'>
+            <a className='author'>Avishka Sathyanjana</a>
+            <time>2023-11-28 16:48</time>
+          </p>
+          <p className='summery'>
+            Generative artificial intelligence has seen an incredible popularity
+            surge in 2022. Big Think has called it ‘the technology of the year’,
+            and judging from the amount of attention and VC support generative
+            AI startups have been gaining this year, this claim is more than
+            justified. Moreover, tech experts say that in the next few years,
+            not only will the development of generative AI not slow down but
+            will also rapidly increase, conquering new and new fields. In this
+            post, written in collaboration with Serokell’s AI developers, I’ll
+            take a closer look at what generative AI is and how it works, as
+            well as outline common use cases and perspectives for the future.
+          </p>
+        </div>
+      </div>
+
+      <div className="post">
+        <div className="image">
+          <img src="https://serokell.io/files/q5/q5c8rtrc.pasted_image_0_(5).png"></img>
+        </div>
+        <div className="texts">
+          <h2>Introduction to Generative AI</h2>
+          <p className='info'>
+            <a className='author'>Avishka Sathyanjana</a>
+            <time>2023-11-28 16:48</time>
+          </p>
+          <p className='summery'>
+            Generative artificial intelligence has seen an incredible popularity
+            surge in 2022. Big Think has called it ‘the technology of the year’,
+            and judging from the amount of attention and VC support generative
+            AI startups have been gaining this year, this claim is more than
+            justified. Moreover, tech experts say that in the next few years,
+            not only will the development of generative AI not slow down but
+            will also rapidly increase, conquering new and new fields. In this
+            post, written in collaboration with Serokell’s AI developers, I’ll
+            take a closer look at what generative AI is and how it works, as
+            well as outline common use cases and perspectives for the future.
+          </p>
+        </div>
+      </div>
+
+      <div className="post">
+        <div className="image">
+          <img src="https://serokell.io/files/q5/q5c8rtrc.pasted_image_0_(5).png"></img>
+        </div>
+        <div className="texts">
+          <h2>Introduction to Generative AI</h2>
+          <p className='info'>
+            <a className='author'>Avishka Sathyanjana</a>
+            <time>2023-11-28 16:48</time>
+          </p>
+          <p className='summery'>
+            Generative artificial intelligence has seen an incredible popularity
+            surge in 2022. Big Think has called it ‘the technology of the year’,
+            and judging from the amount of attention and VC support generative
+            AI startups have been gaining this year, this claim is more than
+            justified. Moreover, tech experts say that in the next few years,
+            not only will the development of generative AI not slow down but
+            will also rapidly increase, conquering new and new fields. In this
+            post, written in collaboration with Serokell’s AI developers, I’ll
+            take a closer look at what generative AI is and how it works, as
+            well as outline common use cases and perspectives for the future.
+          </p>
+        </div>
+      </div>
     </main>
   );
 }
