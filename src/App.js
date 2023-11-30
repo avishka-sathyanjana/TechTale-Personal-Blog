1ace1bcb0a39ec825b4d6e7e6d5681664d9d8de8
Avishka Sathyanjana <baavishka@gmail.com>
2023-11-30 11:38:37 +0530

    feat: adding user context for the frontend

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
 import logo from "./logo.svg";
 import "./App.css";
+
+import {Route, Routes} from "react-router-dom"
+import {UserContextProvider} from "./component/UserContext";
+
 import Post from "./component/Post";
 import Header from "./component/Header";
-import {Route, Routes} from "react-router-dom"
 import Layout from "./component/Layout";
+
 import IndexPage from "./pages/index-page";
 import LoginPage from "./pages/login-page";
 import RegisterPage from "./pages/register-page";
 
 function App() {
   return (
-    <Routes>
-      
-      <Route path="/" element={<Layout/>}>
-        <Route index element={<IndexPage />}/>
-        <Route path={'/login'} element={ <LoginPage />} />  
-        <Route path={'/register'} element={ <RegisterPage />} />
-      </Route>
 
-    </Routes>
+    <UserContextProvider>
+      <Routes>
+        <Route path="/" element={<Layout/>}>
+          <Route index element={<IndexPage />}/>
+          <Route path={'/login'} element={ <LoginPage />} />  
+          <Route path={'/register'} element={ <RegisterPage />} />
+        </Route>
+      </Routes>
+    </UserContextProvider>
+    
   );
 }
 
